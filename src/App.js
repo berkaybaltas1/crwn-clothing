a0545b08a92d85a5cb2225d1b2d46d7b8bd1d080
Berkay Baltas <berkay@Berkays-MacBook-Pro.local>
2021-08-16 02:33:06 -0400

    broken homepage into smaller components and finished styling menu item component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from "react";
+import Homepage from "./pages/homepage/homepage.component";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return <div className='App'>Hello World</div>;
-  }
+function App() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
 }
 
 export default App;
